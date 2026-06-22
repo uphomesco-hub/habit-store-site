e41377cb165cd5a9d35e916b002a25aa34a79e3c
Uphomes <uphomes.co@gmail.com>
2026-06-22 11:19:01 +0530

    Fix Vercel asset base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/habit-store-site/",
+  base: process.env.VERCEL ? "/" : "/habit-store-site/",
   plugins: [react()],
 });
